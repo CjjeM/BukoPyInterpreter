6003903672288a56d1a731ff7ec13dc5dc0ebbdd
jobert <EpicLegitPlayer@gmail.com>
2021-06-02 18:47:11 +0800

    Read files

--- main.py
+++ main.py
@@ -1,38 +1,4 @@
 from BukoPyInterpreter import *
-from BukoPySymbolTable import *
-
-#######################################
-# RUN
-#######################################
-
-global_symbol_table = SymbolTable()
-global_symbol_table.set("null", Number(0))
-global_symbol_table.set("print", BuiltInFunction("print"))
-global_symbol_table.set("False", Number(0))
-global_symbol_table.set("True", Number(1))
-
-
-def run(fn, text):
-    # Generate tokens
-    lexer = Lexer(fn, text)
-    tokens, error = lexer.make_tokens()
-    if error:
-        return None, error
-    print(tokens)
-
-    # Generate AST
-    parser = Parser(tokens)
-    ast = parser.parse()
-    if ast.error:
-        return None, ast.error
-
-    # Run program
-    interpreter = Interpreter()
-    context = Context('<program>')
-    context.symbol_table = global_symbol_table
-    result = interpreter.visit(ast.node, context)
-
-    return result.value, result.error
 
 
 def main():
